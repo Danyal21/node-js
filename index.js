4abe270467d5b93fa6bf338ba7f441e3dcc9adf4
Muhammad Danyal <149050310+Danyal21@users.noreply.github.com>
2023-11-10 16:54:59 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express  from "express";
+import path from "path";
+
+
+const app = express();
+
+app.use(express.static('./'))
+app.get("/getproducts",(req,res)=>{
+res.json({
+    success: true,
+    products:[],
+
+});
+});
+
+const quotes = [
+    "Be a guide, Not a judge.",
+    "Speading a good  word is a virtue.",
+    "An ending is not the end.",
+  ];
+
+app.get('/randomQuote', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    res.json({ randomQuote });
+  });
+
+  app.get('/Cute', (req, res) => {
+    res.json({ message: 'Tur, Ture!' });
+  });
+
+
+  app.get('/', (req, res) => {
+    res.render('index.html');
+  });
+
+
+app.listen(5000, ()=>{
+console.log("Server is working");
+});
