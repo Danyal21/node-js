e21d1655f18c290cf885f5049f1fd037dddee955
Muhammad Danyal <149050310+Danyal21@users.noreply.github.com>
2023-11-15 10:51:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ const expressJwt  = require ('express-jwt');
 
 const app = express();
 app.use(express.static('./'))
-app.get("/getproducts",(req,res)=>{
-    
+app.get("/getproducts",(req,res)=>{    
 const pathlocation = path.resolve();
 res.render('index.html');
 
 });
+
+const users = [
+    { id:1, username: 'user1'; password: 'password1'},
+    { id:2, username: 'user2'; password: 'password2'},
+];
+student.use(new LocalStrategy(
+    (username, password, done)=> {
+        const user = users.find(u =>u.username === && u.password === password); 
+if (user) {
+    return done (null, user);
+}else{
+    return done (null, false, {message: 'Incorerrect username or password'});
+}
+    }
+    ));
 
 const quotes = [
     "Be a guide, Not a judge.",
